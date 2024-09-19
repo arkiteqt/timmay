f4b25ca3478e1cea0f296dea4f725f15ed2c4595
amit <amit@themobidev.org>
2024-09-19 15:23:13 -0400

    init boilerplate code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/styles.scss';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h2>Timmay</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: 'style-loader'
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: 'css-loader'
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  autoprefixer
+                ]
+              }
+            }
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader'
+          }
+        ]
+      },{
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/templates/pages/index.html',
+    //   favicon: './dist/favicon.ico',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".*",".js",".jsx",".scss",".css"]
+  },
+};
